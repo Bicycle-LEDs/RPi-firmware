4399b581d307a492fcd205af9ae3a2f948225380
LukeMech <lukaszjajkommz@gmail.com>
2023-03-22 20:11:47 +0100

    Initial commit

---
+++ modules/getSugarLvl.py
@@ -0,0 +1,35 @@
+from pydexcom import Dexcom
+from gtts import gTTS
+import os
+
+# Login
+dexcom = Dexcom("USERNAME", "PASSWORD", ous=True)
+
+# Get reading
+bg = dexcom.get_current_glucose_reading()
+print("Glucose level: ")
+print(bg.value)
+
+# Create nice trend transcription
+if bg.trend == 1:
+    trend = "bardzo szybko rośnie"
+elif bg.trend == 2:
+    trend = "szybko rośnie"
+elif bg.trend == 3:
+    trend = "powoli wzrasta"
+elif bg.trend == 4:
+    trend = "jest stabilny"
+elif bg.trend == 5:
+    trend = "lekko spada"
+elif bg.trend == 6:
+    trend = "sporawo spada"
+elif bg.trend == 7:
+    trend = "mocno spada"
+else:
+    trend = "są problemy z wyznaczeniem kierunku"
+
+# Read loudly
+tts = gTTS("Twój cukier to " + str(bg.value) + " i " + trend, lang='pl')
+tts.save('dexcom-value.mp3')
+os.system('mpg123 dexcom-value.mp3')
+os.remove('dexcom-value.mp3')
---
+++ modules/speechRecognition.py
@@ -0,0 +1,29 @@
+import time
+import json
+import speech_recognition as sr
+import os
+
+with open('./modules.json') as f:
+    modules = json.load(f)
+
+r = sr.Recognizer()
+# do some more unrelated things
+while True: 
+    with sr.Microphone(device_index=1) as source:
+        audio = r.listen(source)
+        print("Listening!")
+    
+        # received audio data, now we'll recognize it using Google Speech Recognition
+    try:
+        text = r.recognize_google(audio, language='pl-PL').lower()
+        for module in modules:
+                for alias in module["aliases"]:
+                    index = text.find(alias)
+                    if index != -1:
+                        os.system('mpg123 gotIt.mp3')
+                        os.system(module["exec"])
+            
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
