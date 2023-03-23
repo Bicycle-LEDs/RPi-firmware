1d48dd031b053c76165e28c0a4fd49915c27423a
LukeMech <lukaszjajkommz@gmail.com>
2023-03-23 21:21:22 +0100

    Update system-setup.sh

--- config/system-setup.sh
+++ config/system-setup.sh
@@ -6,13 +6,13 @@ echo "FONT=Lat2-Terminus16" | sudo tee -a /etc/vconsole.conf
 echo "FONT_MAP=8859-2" | sudo tee -a /etc/vconsole.conf
 sudo pacman-mirrors --api --set-branch testing
 sudo pacman-mirrors --fasttrack 5 
-sudo pacman -Sy mpg123 portaudio spotifyd neofetch python-pip git base-devel
+sudo pacman -Sy mpg123 portaudio spotifyd neofetch python-pip git base-devel --noconfirm
 
 sudo mkdir /spotify-cache
 sudo loginctl enable-linger $USER
 systemctl --user enable spotifyd.service
 
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 git clone https://aur.archlinux.org/yay-bin
 cd yay-bin
 makepkg -si --noconfirm
