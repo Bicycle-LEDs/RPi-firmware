dc98db72940a9f0e0a9d28cc816f9b8629e52361
LukeMech <lukaszjajkommz@gmail.com>
2023-03-23 19:00:24 +0100

    Add critical errors handling

--- modules/getSugarLvl.py
+++ modules/getSugarLvl.py
@@ -1,49 +1,59 @@
-from pydexcom import Dexcom
-from gtts import gTTS
 import os, json
-script_dir=os.path.dirname(os.path.realpath(__file__))
 
-with open(script_dir + '/../credentials.json') as f:
-    login = json.load(f)
-
-print("[INFO] Łączenie z Dexcom...")
-tts = gTTS("Łączenie z Dexcom...", lang='pl', lang_check=False)
-tts.save('workingOnIt.mp3')
-os.system('setsid mpg123 workingOnIt.mp3 && rm -rf workingOnIt.mp3 >/dev/null 2>&1 < /dev/null &')
-
-# Login
 try:
-    dexcom = Dexcom(login["dexcom"]["login"], login["dexcom"]["password"], ous=login["dexcom"]["OutsideUS"])
-    bg = dexcom.get_current_glucose_reading()
+    from gtts import gTTS
+    from pydexcom import Dexcom
+    script_dir=os.path.dirname(os.path.realpath(__file__))
 
-    # Get reading
-    print("[RESULT] Poziom glukozy: " + str(bg.value) + " - " + bg.trend_description + " (" + bg.trend_arrow + ")")
+    with open(script_dir + '/../credentials.json') as f:
+        login = json.load(f)
 
-    # Create nice trend transcription
-    if bg.trend == 1:
-        trend = "bardzo szybko rośnie"
-    elif bg.trend == 2:
-        trend = "szybko rośnie"
-    elif bg.trend == 3:
-        trend = "powoli wzrasta"
-    elif bg.trend == 4:
-        trend = "stabilnie"
-    elif bg.trend == 5:
-        trend = "lekko spada"
-    elif bg.trend == 6:
-        trend = "sporawo spada"
-    elif bg.trend == 7:
-        trend = "mocno spada"
-    else:
-        trend = "wyznaczenie trendu nie powiodło się"
+    print("[INFO] Łączenie z Dexcom...")
+    tts = gTTS("Łączenie z Dexcom...", lang='pl', lang_check=False)
+    tts.save('workingOnIt.mp3')
+    os.system('setsid mpg123 workingOnIt.mp3 && rm -rf workingOnIt.mp3 >/dev/null 2>&1 < /dev/null &')
 
-    # Read loudly
-    tts = gTTS(str(bg.value) + " i " + trend, lang='pl', lang_check=False)
+    # Login
+    try:
+        dexcom = Dexcom(login["dexcom"]["login"], login["dexcom"]["password"], ous=login["dexcom"]["OutsideUS"])
+        bg = dexcom.get_current_glucose_reading()
+
+        # Get reading
+        print("[RESULT] Poziom glukozy: " + str(bg.value) + " - " + bg.trend_description + " (" + bg.trend_arrow + ")")
+
+        # Create nice trend transcription
+        if bg.trend == 1:
+            trend = "bardzo szybko rośnie"
+        elif bg.trend == 2:
+            trend = "szybko rośnie"
+        elif bg.trend == 3:
+            trend = "powoli wzrasta"
+        elif bg.trend == 4:
+            trend = "stabilnie"
+        elif bg.trend == 5:
+            trend = "lekko spada"
+        elif bg.trend == 6:
+            trend = "sporawo spada"
+        elif bg.trend == 7:
+            trend = "mocno spada"
+        else:
+            trend = "wyznaczenie trendu nie powiodło się"
+
+        # Read loudly
+        tts = gTTS(str(bg.value) + " i " + trend, lang='pl', lang_check=False)
+
+    except:
+        tts = gTTS("Połączenie nieudane", lang='pl', lang_check=False)
+
+    tts.save('dexcom-value.mp3')
+    os.system('mpg123 dexcom-value.mp3')
+    os.remove('dexcom-value.mp3')
+    print("[INFO] Wykonano skrypt.")
 
 except:
-    tts = gTTS("Połączenie nieudane", lang='pl', lang_check=False)
-
-tts.save('dexcom-value.mp3')
-os.system('mpg123 dexcom-value.mp3')
-os.remove('dexcom-value.mp3')
-print("[INFO] Wykonano skrypt.")
+    print("[ERR] Wykonywanie skryptu nieudane.")
+    from gtts import gTTS
+    tts = gTTS("Wystąpił krytyczny błąd w skrypcie", lang='pl', lang_check=False)
+    tts.save('response.mp3')
+    os.system('mpg123 response.mp3')
+    os.remove('response.mp3')
