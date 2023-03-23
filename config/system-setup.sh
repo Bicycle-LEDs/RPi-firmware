a23d15ca4c3bad9bdd9a4b283c72c86fac4095e7
LukeMech <lukaszjajkommz@gmail.com>
2023-03-23 21:13:26 +0100

    Update system-setup.sh

--- config/system-setup.sh
+++ config/system-setup.sh
@@ -4,7 +4,7 @@ echo ! CONFIGURING BASE SYSTEM !
 
 echo "FONT=Lat2-Terminus16" | sudo tee -a /etc/vconsole.conf
 echo "FONT_MAP=8859-2" | sudo tee -a /etc/vconsole.conf
-sudo pacman-mirrors --api --set-branch testging
+sudo pacman-mirrors --api --set-branch testing
 sudo pacman-mirrors --fasttrack 5 
 sudo pacman -Syy linux-rpi4-mainline linux-rpi4-mainline-headers --noconfirm
 sudo pacman -Sy mpg123 portaudio spotifyd neofetch python-pip git base-devel
