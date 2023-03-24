274211da871af766be8c051a1d5270b02c106bca
LukeMech <lukaszjajkommz@gmail.com>
2023-03-24 17:39:56 +0100

    Update textToSpeech.py

--- app/modules/helpers/textToSpeech.py
+++ app/modules/helpers/textToSpeech.py
@@ -4,28 +4,29 @@ import sys, random, string, os
 def tts(language, message):
     # try: 
         # Try importing text-to-speech
-        from gtts import gTTS
+    from gtts import gTTS
 
-        # Generate random string for filename - to prevent multi-script-running issues
-        digits = random.choices(string.digits, k=3)
-        letters = random.choices(string.ascii_letters, k=6)
-        filename = random.sample(digits + letters, 9) + '.mp3'
+    # Generate random string for filename - to prevent multi-script-running issues
+    digits = random.choices(string.digits, k=3)
+    letters = random.choices(string.ascii_letters, k=6)
+    filename = random.sample(digits + letters, 9) + '.mp3'
 
-        # Generate audio file in argument 1 language and saying argument 2 text 
-        TTS = gTTS(message, lang=language, lang_check=False)
-        TTS.save('../temp/' + filename)
-        print(filename)
-        # Read audio file
-        os.system('mpg123 ../temp/' + filename)
-        os.remove('../temp/' + filename)
-        return True
+    # Generate audio file in argument 1 language and saying argument 2 text 
+    TTS = gTTS(message, lang=language, lang_check=False)
+    TTS.save('../temp/' + filename)
+    print(filename)
+    # Read audio file
+    os.system('mpg123 ../temp/' + filename)
+    os.remove('../temp/' + filename)
+    return True
     
     # except:
     #     # Return error
     #     return False
 
 # Run function if called as file
-try: 
-    tts(sys.argv[1], sys.argv[2])
-except:
-    pass
+# try: 
+tts(sys.argv[1], sys.argv[2])
+# except:
+#     print("ERROR")
+#     pass
