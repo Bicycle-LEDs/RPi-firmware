d9832cc073b80103ff2b4741576464a23a2247e9
LukeMech <lukaszjajkommz@gmail.com>
2023-03-24 22:25:37 +0100

    Repair scripts

--- app/modules/bingChat.py
+++ app/modules/bingChat.py
@@ -10,61 +10,58 @@ infoMsg = colorama.Fore.GREEN + "[CHATGPT] " + colorama.Style.RESET_ALL
 errorMsg = colorama.Fore.RED + "[CHATGPT] " + colorama.Style.RESET_ALL
 ctrlCMsg = "\n" + infoMsg + "Użyto" + colorama.Fore.RED + " Ctrl + C" + colorama.Style.RESET_ALL + ", wyjście do nadrzędnego skryptu"
 
-def main():
-    try:
-        # Import chatgpt library
-        from EdgeGPT import Chatbot, ConversationStyle
+try:
+    # Import chatgpt library
+    from EdgeGPT import Chatbot, ConversationStyle
 
-        # Import speechrecognition and tts scripts
-        from helpers.speechRecognition import speechRecognition
-        from helpers.textToSpeech import tts
+    # Import speechrecognition and tts scripts
+    from helpers.speechRecognition import speechRecognition
+    from helpers.textToSpeech import tts
 
-        # Play sound
-        os.system(F'setsid mpg123 {script_dir}/../sounds/gotIt.mp3 >/dev/null')
-        print(infoMsg + "Uruchamianie rozpoznawania mowy...")
+    # Play sound
+    os.system(F'setsid mpg123 {script_dir}/../sounds/gotIt.mp3 >/dev/null')
+    print(infoMsg + "Uruchamianie rozpoznawania mowy...")
 
-        # Recognize voice
-        text = speechRecognition('pl-PL')
+    # Recognize voice
+    text = speechRecognition('pl-PL')
 
-        # If unknown value or module error play error sound
-        if text == 1 or text == 2 or text == False:
-            os.system(F'setsid mpg123 {script_dir}/sounds/connectionError.mp3 >/dev/null')
+    # If unknown value or module error play error sound
+    if text == 1 or text == 2 or text == False:
+        os.system(F'setsid mpg123 {script_dir}/sounds/connectionError.mp3 >/dev/null')
 
-        # If speech recognized     
-        else:
+    # If speech recognized     
+    else:
 
-            # Open credentials file
-            with open(script_dir + '/../credentials.json') as f:
-                login = json.load(f)
+        # Open credentials file
+        with open(script_dir + '/../credentials.json') as f:
+            login = json.load(f)
 
-            try:
-                # Authorize to bing ai
-                print(infoMsg + "Logowanie do bingchat...")
-                bot = Chatbot(cookies=login["bingchat"]["cookies"])
+        try:
+            # Authorize to bing ai
+            print(infoMsg + "Logowanie do bingchat...")
+            bot = Chatbot(cookies=login["bingchat"]["cookies"])
 
-                # Generate response
-                print(infoMsg + "(TTS) Łączenie z bing chat i generowanie odpowiedzi...")
-                os.system(F'python {script_dir}/helpers/textToSpeech.py pl "Zaczekaj na odpowiedź"')
-                message = bot.ask(prompt=text + " - ogranicz odpowiedź do 30 słów", conversation_style=ConversationStyle.balanced)
-                bot.close()
-                print(infoMsg + "(TTS) Odpowiedź: " + colorama.Fore.CYAN + message)
-                if tts('pl', message) == 3:
-                    print(ctrlCMsg)
-                    return 3
+            # Generate response
+            print(infoMsg + "(TTS) Łączenie z bing chat i generowanie odpowiedzi...")
+            os.system(F'python {script_dir}/helpers/textToSpeech.py pl "Zaczekaj na odpowiedź"')
+            message = bot.ask(prompt=text + " - ogranicz odpowiedź do 30 słów", conversation_style=ConversationStyle.balanced)
+            bot.close()
+            print(infoMsg + "(TTS) Odpowiedź: " + colorama.Fore.CYAN + message)
+            if tts('pl', message) == 3:
+                print(ctrlCMsg)
 
-            except:
-                # Problem connecting / generating response
-                print(errorMsg + "(TTS) Połączenie z czatbotem nieudane")
-                if tts('pl', "Połączenie nieudane") == 3:
-                    print(ctrlCMsg)
+        except:
+            # Problem connecting / generating response
+            print(errorMsg + "(TTS) Połączenie z czatbotem nieudane")
+            if tts('pl', "Połączenie nieudane") == 3:
+                print(ctrlCMsg)
 
 
-    # Ctrl + C handle
-    except KeyboardInterrupt:
-        print(ctrlCMsg)
-        return 3
-    
-    # Critical error
-    except:
-        print(errorMsg + "Wystąpił nieprzewidziany błąd w skrypcie")
-        os.system(F'mpg123 {script_dir}/../sounds/scriptError.mp3')
+# Ctrl + C handle
+except KeyboardInterrupt:
+    print(ctrlCMsg)
+
+# Critical error
+except:
+    print(errorMsg + "Wystąpił nieprzewidziany błąd w skrypcie")
+    os.system(F'mpg123 {script_dir}/../sounds/scriptError.mp3')
--- app/modules/chatGpt.py
+++ app/modules/chatGpt.py
@@ -10,61 +10,58 @@ infoMsg = colorama.Fore.GREEN + "[CHATGPT] " + colorama.Style.RESET_ALL
 errorMsg = colorama.Fore.RED + "[CHATGPT] " + colorama.Style.RESET_ALL
 ctrlCMsg = "\n" + infoMsg + "Użyto" + colorama.Fore.RED + " Ctrl + C" + colorama.Style.RESET_ALL + ", wyjście do nadrzędnego skryptu"
 
-def main():
-    try:
-        # Import chatgpt library
-        import openai
+try:
+    # Import chatgpt library
+    import openai
 
-        # Import speechrecognition and tts scripts
-        from helpers.speechRecognition import speechRecognition
-        from helpers.textToSpeech import tts
+    # Import speechrecognition and tts scripts
+    from helpers.speechRecognition import speechRecognition
+    from helpers.textToSpeech import tts
 
-        # Play sound
-        os.system(F'setsid mpg123 {script_dir}/../sounds/gotIt.mp3 >/dev/null')
-        print(infoMsg + "Uruchamianie rozpoznawania mowy...")
+    # Play sound
+    os.system(F'setsid mpg123 {script_dir}/../sounds/gotIt.mp3 >/dev/null')
+    print(infoMsg + "Uruchamianie rozpoznawania mowy...")
 
-        # Recognize voice
-        text = speechRecognition('pl-PL')
+    # Recognize voice
+    text = speechRecognition('pl-PL')
 
-        # If unknown value or module error play error sound
-        if text == 1 or text == 2 or text == False:
-            os.system(F'setsid mpg123 {script_dir}/sounds/connectionError.mp3 >/dev/null')
+    # If unknown value or module error play error sound
+    if text == 1 or text == 2 or text == False:
+        os.system(F'setsid mpg123 {script_dir}/sounds/connectionError.mp3 >/dev/null')
 
-        # If speech recognized     
-        else:
+    # If speech recognized     
+    else:
 
-            # Open credentials file
-            with open(script_dir + '/../credentials.json') as f:
-                login = json.load(f)
+        # Open credentials file
+        with open(script_dir + '/../credentials.json') as f:
+            login = json.load(f)
 
-            try:
-                # Authorize to openai chatgpt
-                print(infoMsg + "Logowanie do openai...")
-                openai.api_key = login["openai"]["apiKey"]
+        try:
+            # Authorize to openai chatgpt
+            print(infoMsg + "Logowanie do openai...")
+            openai.api_key = login["openai"]["apiKey"]
 
-                # Generate response
-                print(infoMsg + "(TTS) Łączenie z chatgpt i generowanie odpowiedzi...")
-                os.system(F'python {script_dir}/helpers/textToSpeech.py pl "Zaczekaj na odpowiedź"')
-                completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": text + " - ogranicz odpowiedź do 30 słów"}], max_tokens=100)
-                message = completion.choices[0].message.content
-                print(infoMsg + "(TTS) Odpowiedź: " + colorama.Fore.CYAN + message)
-                if tts('pl', message) == 3:
-                    print(ctrlCMsg)
-                    return 3
+            # Generate response
+            print(infoMsg + "(TTS) Łączenie z chatgpt i generowanie odpowiedzi...")
+            os.system(F'python {script_dir}/helpers/textToSpeech.py pl "Zaczekaj na odpowiedź"')
+            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": text + " - ogranicz odpowiedź do 30 słów"}], max_tokens=100)
+            message = completion.choices[0].message.content
+            print(infoMsg + "(TTS) Odpowiedź: " + colorama.Fore.CYAN + message)
+            if tts('pl', message) == 3:
+                print(ctrlCMsg)
 
-            except:
-                # Problem connecting / generating response
-                print(errorMsg + "(TTS) Połączenie z czatbotem nieudane")
-                if tts('pl', "Połączenie nieudane") == 3:
-                    print(ctrlCMsg)
+        except:
+            # Problem connecting / generating response
+            print(errorMsg + "(TTS) Połączenie z czatbotem nieudane")
+            if tts('pl', "Połączenie nieudane") == 3:
+                print(ctrlCMsg)
 
 
-    # Ctrl + C handle
-    except KeyboardInterrupt:
-        print(ctrlCMsg)
-        return 3
-    
-    # Critical error
-    except:
-        print(errorMsg + "Wystąpił nieprzewidziany błąd w skrypcie")
-        os.system(F'mpg123 {script_dir}/../sounds/scriptError.mp3')
+# Ctrl + C handle
+except KeyboardInterrupt:
+    print(ctrlCMsg)
+
+# Critical error
+except:
+    print(errorMsg + "Wystąpił nieprzewidziany błąd w skrypcie")
+    os.system(F'mpg123 {script_dir}/../sounds/scriptError.mp3')
