e52fab8479a94ec93113eca9c290ded68247ae71
LukeMech <lukaszjajkommz@gmail.com>
2023-03-24 19:03:17 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -8,11 +8,11 @@ infoMsg = Fore.GREEN + "[LAUNCH] " + Style.RESET_ALL
 print(infoMsg + " Tak będą wyglądać informacje")
 print(Fore.YELLOW + "[LAUNCH]" + Style.RESET_ALL + " Tak ostrzeżenia")
 print(Fore.RED + "[LAUNCH]" + Style.RESET_ALL + " A tak błędy")
+print()
 
 # Update
 if not len(sys.argv) > 1:
     print(infoMsg + " Aktualizowanie używając polecenia " + Fore.YELLOW + "git pull")
-    print()
     print("----------" + Style.RESET_ALL)
     os.system('git pull')
     print(Fore.YELLOW + "----------")
@@ -22,4 +22,4 @@ if not len(sys.argv) > 1:
 print(infoMsg + " Uruchamianie " + Fore.YELLOW + "main.py")
 print(" ")
 
-os.system(F'python {script_dir}/app/main.py 2>/dev/null')
+os.system(F'python {script_dir}/app/main.py')
