def582bfff19b9b94c4bd1c83babff1696539b59
LukeMech <lukaszjajkommz@gmail.com>
2023-03-24 22:53:30 +0100

    Update bingChat-debug.py

--- app/modules/bingChat-debug.py
+++ app/modules/bingChat-debug.py
@@ -23,7 +23,7 @@ os.system(F'setsid mpg123 {script_dir}/../sounds/gotIt.mp3 >/dev/null')
 print(infoMsg + "Uruchamianie rozpoznawania mowy...")
 
 # Recognize voice
-text = "ile to 2 + 2"
+text = "co jedzą żółwie"
 
 # If unknown value or module error play error sound
 if text == 1 or text == 2 or text == False:
