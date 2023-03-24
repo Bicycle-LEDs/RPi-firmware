b195378dc629d184378c0462deef7919333ecb58
LukeMech <lukaszjajkommz@gmail.com>
2023-03-24 19:42:01 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -5,6 +5,8 @@ script_dir=os.path.dirname(os.path.realpath(__file__))
 init()
 
 infoMsg = Fore.GREEN + "[LAUNCH] " + Style.RESET_ALL
+startscriptMsg = Fore.YELLOW + "--------------------" + Style.RESET_ALL
+endScriptMsg = Fore.BLUE + "--------------------" + Style.RESET_ALL
 
 print(infoMsg + " Tak będą wyglądać informacje")
 print(Fore.YELLOW + "[LAUNCH]" + Style.RESET_ALL + " Tak ostrzeżenia")
@@ -14,22 +16,22 @@ print()
 # Update
 if not len(sys.argv) > 1:
     print(infoMsg + " Aktualizowanie systemu używając polecenia " + Fore.YELLOW + "yay -Syu")
-    print("----------" + Style.RESET_ALL)
+    print(startscriptMsg)
     os.system('yay -Syu')
-    print(Fore.YELLOW + "----------")
-    print()
+    print(Fore.YELLOW + "--------------------")
+    print(endScriptMsg)
 
     print(infoMsg + " Aktualizowanie kodu używając polecenia " + Fore.YELLOW + "git pull")
-    print("----------" + Style.RESET_ALL)
+    print(startscriptMsg)
     os.system('git pull')
-    print(Fore.YELLOW + "----------")
+    print(endScriptMsg)
     print()
 
     print(infoMsg + " Aktualizowanie bibliotek używając polecenia " + Fore.YELLOW + "pip install -U")
-    print("----------" + Style.RESET_ALL)
+    print(startscriptMsg)
     packages = [dist.project_name for dist in pkg_resources.working_set]
     os.system("pip install --upgrade " + ' '.join(packages))
-    print(Fore.YELLOW + "----------")
+    print(endScriptMsg)
     print()
 
 # Run
