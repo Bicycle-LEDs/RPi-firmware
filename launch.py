61a69a64e8fb8963a9f1068267b6d5a23ae0879f
LukeMech <lukaszjajkommz@gmail.com>
2023-03-24 19:01:50 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -3,18 +3,23 @@ from colorama import init, Fore, Style
 script_dir=os.path.dirname(os.path.realpath(__file__))
 init()
 
-# Launch
-if len(sys.argv) > 1 and sys.argv[1] == 'launch':
+infoMsg = Fore.GREEN + "[LAUNCH] " + Style.RESET_ALL
 
-    print(Fore.GREEN + "[LAUNCH]" + Style.RESET_ALL + " Tak będą wyglądać informacje")
-    print(Fore.YELLOW + "[LAUNCH]" + Style.RESET_ALL + " Tak ostrzeżenia")
-    print(Fore.RED + "[LAUNCH]" + Style.RESET_ALL + " A tak błędy")
-    print(Fore.GREEN + "[LAUNCH]" + Style.RESET_ALL + " Uruchamianie " + Fore.YELLOW + "main.py")
-    print(" ")
-
-    os.system(F'python {script_dir}/app/main.py 2>/dev/null')
+print(infoMsg + " Tak będą wyglądać informacje")
+print(Fore.YELLOW + "[LAUNCH]" + Style.RESET_ALL + " Tak ostrzeżenia")
+print(Fore.RED + "[LAUNCH]" + Style.RESET_ALL + " A tak błędy")
 
 # Update
-else:
+if not len(sys.argv) > 1:
+    print(infoMsg + " Aktualizowanie używając polecenia " + Fore.YELLOW + "git pull")
+    print()
+    print("----------" + Style.RESET_ALL)
     os.system('git pull')
-    os.system(F'setsid python {script_dir}/launch.py launch >/dev/null 2>&1 < /dev/null &')
+    print(Fore.YELLOW + "----------")
+    print()
+
+# Run
+print(infoMsg + " Uruchamianie " + Fore.YELLOW + "main.py")
+print(" ")
+
+os.system(F'python {script_dir}/app/main.py 2>/dev/null')
