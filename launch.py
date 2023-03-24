c60f9f52e0ae97b5101d29e420b9cf3c57b0e635
LukeMech <lukaszjajkommz@gmail.com>
2023-03-24 19:58:41 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -8,7 +8,7 @@ infoMsg = Fore.GREEN + "[LAUNCH] " + Style.RESET_ALL
 startscriptMsg = Fore.YELLOW + "--------------------" + Style.RESET_ALL
 endScriptMsg = Fore.BLUE + "--------------------" + Style.RESET_ALL
 
-print(infoMsg + " Tak będą wyglądać informacje")
+print(infoMsg + "Tak będą wyglądać informacje")
 print(Fore.YELLOW + "[LAUNCH]" + Style.RESET_ALL + " Tak ostrzeżenia")
 print(Fore.RED + "[LAUNCH]" + Style.RESET_ALL + " A tak błędy")
 print()
@@ -16,19 +16,19 @@ print()
 # Update
 if not len(sys.argv) > 1:
     
-    print(infoMsg + " Aktualizowanie systemu używając polecenia " + Fore.YELLOW + "yay -Syu")
+    print(infoMsg + "Aktualizowanie systemu używając polecenia " + Fore.YELLOW + "yay -Syu")
     print(startscriptMsg)
     os.system('yay -Syu')
     print(endScriptMsg)
     print()
 
-    print(infoMsg + " Aktualizowanie kodu używając polecenia " + Fore.YELLOW + "git pull")
+    print(infoMsg + "Aktualizowanie kodu używając polecenia " + Fore.YELLOW + "git pull")
     print(startscriptMsg)
     os.system('git pull')
     print(endScriptMsg)
     print()
 
-    print(infoMsg + " Aktualizowanie bibliotek używając polecenia " + Fore.YELLOW + "pip install -U")
+    print(infoMsg + "Aktualizowanie bibliotek używając polecenia " + Fore.YELLOW + "pip install -U")
     print(startscriptMsg)
     packages = [dist.project_name for dist in pkg_resources.working_set]
     os.system("pip install --upgrade " + ' '.join(packages))
@@ -36,6 +36,6 @@ if not len(sys.argv) > 1:
     print()
 
 # Run
-print(infoMsg + " Uruchamianie " + Fore.YELLOW + "main.py")
+print(infoMsg + "Uruchamianie " + Fore.YELLOW + "main.py")
 
 os.system(F'python {script_dir}/app/main.py 2>/dev/null')
