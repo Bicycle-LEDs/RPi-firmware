f8e9c8c5989f5148b17dae04e35bdcb4224979f3
LukeMech <lukaszjajkommz@gmail.com>
2023-03-25 11:25:22 +0100

    Update OLEDRefresh.py

--- app/modules/helpers/OLEDRefresh.py
+++ app/modules/helpers/OLEDRefresh.py
@@ -10,59 +10,54 @@ infoMsg = colorama.Fore.GREEN + "[OLED] " + colorama.Style.RESET_ALL
 warningMsg = colorama.Fore.YELLOW + "[OLED] " + colorama.Style.RESET_ALL
 errorMsg = colorama.Fore.RED + "[OLED] " + colorama.Style.RESET_ALL
 ctrlCMsg = "\n" + infoMsg + "Użyto" + colorama.Fore.RED + " Ctrl + C" + colorama.Style.RESET_ALL + ", wyjście do nadrzędnego skryptu"
+   
+def OLEDRefresh(text="Słucham..."):
+    try: 
+         # Import required packages
+        import board, displayio, terminalio, adafruit_displayio_ssd1306
+        from adafruit_display_text import label
 
-try:
-    # Import required packages
-    import board, displayio, terminalio, adafruit_displayio_ssd1306
-    from adafruit_display_text import label
+        # Open settings file
+        with open(script_dir + '/../../settings.json') as f:
+            settings = json.load(f)["GPIO"]["OLEDScreen"]
+        border=5
 
-    # Open settings file
-    with open(script_dir + '/../../settings.json') as f:
-        settings = json.load(f)["GPIO"]["OLEDScreen"]
-    border=5
+        # Initialize screen
+        displayio.release_displays()
+        i2c = board.I2C()
+        display_bus = displayio.I2CDisplay(i2c, device_address=settings["address"])
+        display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=settings["width"], height=settings["height"])
 
-    # Initialize screen
-    displayio.release_displays()
-    i2c = board.I2C()
-    display_bus = displayio.I2CDisplay(i2c, device_address=settings["address"])
-    display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=settings["width"], height=settings["height"])
-    splash = displayio.Group()
-    display.show(splash)
-    color_bitmap = displayio.Bitmap(settings["width"], settings["height"], 1)
-    color_palette = displayio.Palette(1)
-    color_palette[0] = 0xFFFFFF
-    OLEDInitialized = True
+        if not display: return
 
-    def OLEDRefresh(text="Listening!"):
-        try: 
-            # Draw text
-            bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-            splash.append(bg_sprite)
+        splash = displayio.Group()
+        display.show(splash)
+        color_bitmap = displayio.Bitmap(settings["width"], settings["height"], 1)
+        color_palette = displayio.Palette(1)
+        color_palette[0] = 0xFFFFFF
 
-            inner_bitmap = displayio.Bitmap(settings["width"]-border*2, settings["height"]-border*2, 1)
-            inner_palette = displayio.Palette(1)
-            inner_palette[0] = 0x000000 # Black
-            inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=border, y=border)
-            splash.append(inner_sprite)
+        # Draw text
+        bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+        splash.append(bg_sprite)
 
-            text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=28, y=settings["height"]//2-1)
-            splash.append(text_area)
+        inner_bitmap = displayio.Bitmap(settings["width"]-border*2, settings["height"]-border*2, 1)
+        inner_palette = displayio.Palette(1)
+        inner_palette[0] = 0x000000 # Black
+        inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=border, y=border)
+        splash.append(inner_sprite)
 
-            display.show(displayio.Group())
-            text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=28, y=settings["height"]//2-1)
+        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=28, y=settings["height"]//2-1)
+        splash.append(text_area)
 
-        # Ctrl + C clicked
-        except KeyboardInterrupt:
-            print(ctrlCMsg)
-            return 3
+        display.show(displayio.Group())
+        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=28, y=settings["height"]//2-1)
 
-        # Critical error
-        except:
-            if OLEDInitialized:
-                print(errorMsg + "Wystąpił nieprzewidziany błąd w skrypcie")
-                return False
-            else: pass
-
-except:
-    print(errorMsg + "Inizjalizacja OLED nieudana")
-    OLEDInitialized = False
+    # Ctrl + C clicked
+    except KeyboardInterrupt:
+        print(ctrlCMsg)
+        return 3
+    
+    # Critical error
+    except:
+        print(errorMsg + "Inizjalizacja OLED nieudana")
+        return False
